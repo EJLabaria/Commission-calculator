51be8a4968d951b7e0eae5d3e283ac29c5adca0c
EJ Labaria <43556136+EJLabaria@users.noreply.github.com>
2018-12-12 23:43:39 -0800

    Update commission-calc.py

--- commission-calc.py
+++ commission-calc.py
@@ -1,6 +1,19 @@
+#************************************************************
+#                                                           *
+#   Commission Calculator - Monolithic - Python Practice    *
+#      by: EJ Labaria  12.12.18                             *
+#************************************************************
+
 #levels: TE, TR, SR, PR, 
 
+# TE = 55, 7.5%
+# TR = 65, 7.5%
+# SR = 75, 12.5%
+# PR = 85, 12.5%
 
 
 print("What's your current level?")
 title = input()
+print(title + "! Very nice :) ")
+
+if (title = TE)
